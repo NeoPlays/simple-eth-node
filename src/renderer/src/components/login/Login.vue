99939fa45303b452a2d16b65db8561db3df76bc9
NeoPlays <80448387+NeoPlays@users.noreply.github.com>
2026-06-23 16:33:24 +0200

    FIX: login screen styling

--- src/renderer/src/components/login/Login.vue
+++ src/renderer/src/components/login/Login.vue
@@ -14,9 +14,14 @@ import SSHServerList from './SSHServerList.vue'
 .login-layout {
     display: grid;
     grid-template-columns: 1fr 1.6fr;
+    grid-template-rows: minmax(0, 1fr);
     width: 100%;
     height: 100%;
     padding: 24px;
     gap: 16px;
+}
+
+.panel {
+    min-height: 0;
 }
 </style>
